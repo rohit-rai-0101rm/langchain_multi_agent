5d6f98d395fef6cd28432394e8431b63d5666abe
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2026-09-09 03:22:12 +0530

    feat(tools):added web_search_tool | 2026-09-08 | 03:22

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.tools.tools import web_search
+
+
+web_search("Latest news on AI research")
+
---
+++ src/tools/tools.py
@@ -0,0 +1,27 @@
+from langchain.tools import tool
+import requests
+
+import os
+from dotenv import load_dotenv
+from tavily import TavilyClient
+
+
+load_dotenv()
+
+tavily=TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+def web_search(query:str)->str:
+    """
+    Search the web for recent and relaible information ona topic.Return Titles,URL ,Snippets and contents
+    """
+    results=tavily.search(query=query,max_results=5)
+
+    print(results)
+
+    out=[]
+    for r in results["results"]:
+        out.append(
+        f"Title:{r['title']}\nURL:{r['url']}\nSnippet:{r['content'][:300]}\n"
+        )
+
+    return "\n-----\n".join(out)
