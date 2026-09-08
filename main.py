795b6f3bba45c95581eed3f5eab1b341110c822b
rohit-rai-0101rm <rohitrai.dev@gmail.com>
2026-09-09 03:39:07 +0530

    feat(web_scrapper_tool): scrapped data using beautifulsoup | 2026-09-09| 03:39

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.tools.tools import web_search
 
+from src.tools.tools import scrape_url
 
-output=web_search("Latest news on AI research")
+output=scrape_url('https://techcrunch.com/category/artificial-intelligence')
+# output=web_search("Latest news on AI research")
 print(output)
